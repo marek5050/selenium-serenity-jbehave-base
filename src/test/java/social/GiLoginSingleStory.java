d082cca22725bc7f668272a870d4a60a3c25b9de
Marek <marek.bejda@gmail.com>
2015-08-26 21:51:44 -0500

    Added new configurations

---
+++ src/test/java/social/GiLoginSingleStory.java
@@ -0,0 +1,10 @@
+package social;
+
+import net.serenitybdd.jbehave.SerenityStory;
+import net.thucydides.jbehave.ThucydidesJUnitStory;
+
+/**
+ * Created by marek5050 on 8/26/15.
+ */
+public class GiLoginSingleStory extends SerenityStory {
+}
